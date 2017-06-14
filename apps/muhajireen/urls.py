7ffecf0b359e5091ca18f7b037707e17474c48fc
agheith <amerghth@gmail.com>
2017-06-14 04:05:12 -0700

    Complete About Us Page

--- apps/muhajireen/urls.py
+++ apps/muhajireen/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^masjid_info$', views.masjid_info),
 ]
--- apps/muhajireen/views.py
+++ apps/muhajireen/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'muhajireen/index.html')
+
+def masjid_info(request):
+    return render(request, 'muhajireen/masjid_info.html')
