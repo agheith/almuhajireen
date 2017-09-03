88ca73a8bca3b9fc83e13e9c6d383afab5f3adf6
Shadman Ahmed <shadman.ahmed001@gmail.com>
2017-09-03 14:11:19 -0700

    merging

--- apps/muhajireen/urls.py
+++ apps/muhajireen/urls.py
@@ -2,6 +2,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index),
     url(r'^masjid_info$', views.masjid_info),
+    url(r'^faq$', views.faq),
+    url(r'^vistus$', views.vistus),
+    url(r'^sermons$', views.sermons),
+    url(r'^programs$', views.programs),
+    url(r'^sundayschool$', views.sundayschool),
+    url(r'^contact$', views.contact),
+    url(r'^donate$', views.donate),
+
+
 ]
--- apps/muhajireen/views.py
+++ apps/muhajireen/views.py
@@ -6,3 +6,25 @@ def index(request):
 
 def masjid_info(request):
     return render(request, 'muhajireen/masjid_info.html')
+
+# we need to edit the following pages SA>
+def faq(request):
+    return render(request, 'muhajireen/about.html')
+
+def vistus(request):
+    return render(request, 'muhajireen/about.html')
+
+def sermons(request):
+    return render(request, 'muhajireen/sermons.html')
+
+def programs(request):
+    return render(request, 'muhajireen/event.html')
+
+def sundayschool(request):
+    return render(request, 'muhajireen/gallery-3-col.html')
+
+def contact(request):
+    return render(request, 'muhajireen/contact.html')
+
+def donate(request):
+    return render(request, 'muhajireen/donation.html')
